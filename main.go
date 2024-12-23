fff79351998fa6f87d4cc0621705699ab5155d4c
Anubhav <anubhavsamanta023@gmail.com>
2024-12-23 09:39:32 +0530

    chat server: first iteration

--- main.go
+++ main.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 )
 
+const PORT = "8080"
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	msg := fmt.Sprintf("Welcome Soldier !\n")
+	n, err := conn.Write([]byte(msg))
+	if err != nil {
+		log.Printf("ERROR: could not write to %s: %s\n", conn.RemoteAddr().String(), err)
+		return
+	}
+
+	if n < len(msg) {
+		log.Printf("The message was not fully written %d/%d\n", n, len(msg))
+	}
+}
+
 func main() {
-	fmt.Println("Hello Go Chat !")
+	ln, err := net.Listen("tcp", ":"+PORT)
+	if err != nil {
+		log.Fatalf("ERROR: could not listen to port %s: %s\n", PORT, err)
+	}
+	fmt.Println("Server is running on PORT:", 8080)
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("ERROR: cound not accept a connection: %s\n", err)
+		}
+		log.Printf("Accepted connection from %s:", conn.RemoteAddr().String())
+		go handleConnection(conn)
+	}
 }
